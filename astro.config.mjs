b75481efe7f417475fc31379df1dc29b0826e115
Fadi <fadikirbag5@gmail.com>
2025-07-01 20:10:00 -0400

    primer deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    devToolbar:{
+        enabled: false
+    },
+
+    vite: {
+        server:{
+            allowedHosts: [
+                'dj34w9-ip-190-153-6-203.tunnelmole.net'
+            ]
+        }
+    },
+
+
+    output: 'server'
+});
